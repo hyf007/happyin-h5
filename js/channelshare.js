04b47733ab24bda17f77fe0e63b3c997819a3966
pokola <13811708195@163.com>
2016-09-01 09:17:49 +0800

    add channel

--- js/channelshare.js
+++ js/channelshare.js
@@ -31,15 +31,20 @@ var environment = {
 };
 
 var downloadHref = '';
-var phone;
-if(environment.isAndroid){
-	downloadHref = 'http://happyin-10041765.file.myqcloud.com/apk/toutiao-release-1.0.7.apk';
-	phone = 'a';
+var phone = '';
+if(getQueryStringArgs().channel == 'toutiao'){
+	if(environment.isAndroid){
+		downloadHref = 'http://happyin-10041765.file.myqcloud.com/apk/toutiao-release-1.0.7.apk';
+		phone = '_a';
+	}
+	if(environment.isIos){
+		downloadHref = 'https://api.happyin.com.cn/Catalog/Stat/adClick?d=59d40fe4&t=59d40fe4-5ba5-4a1b-a195-1dd238901b90';
+		phone = '_i';
+	}
+}else {
+	downloadHref = 'http://a.app.qq.com/o/simple.jsp?pkgname=com.happyin.print';
 }
-if(environment.isIos){
-	downloadHref = 'https://api.happyin.com.cn/Catalog/Stat/adClick?d=59d40fe4&t=59d40fe4-5ba5-4a1b-a195-1dd238901b90';
-	phone = 'i';
-}
+
 
 //var microDownloadUrl = 'http://a.app.qq.com/o/simple.jsp?pkgname=com.happyin.print';	//微下载地址
 function toDownload(){
@@ -47,7 +52,7 @@ function toDownload(){
 		url: location.protocol + '//' + location.host + '/Catalog/Stat/shareDow',
 		dataType: 'text',
 		data: {
-			target: getQueryStringArgs().channel + '_' + phone
+			target: getQueryStringArgs().channel + '' + phone
 		},
 		success: function(d){
 			//console.log(d);
@@ -66,7 +71,7 @@ function forStat() {
 		dataType: 'text',
 		data: {
 			stat: 1,
-			target: getQueryStringArgs().channel + '_' + phone
+			target: getQueryStringArgs().channel + '' + phone
 		},
 		success: function(d){
 
